0fa6fe4db686c2da6d8ecbfb75ad8b92f81fbd04
Abubacer <azizdesignislife@gmail.com>
2023-12-13 17:45:45 +0000

    tests docstrings

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,5 @@
 import pytest
+""" Test cases module """
 from app import app, flower_data, filter_flowers, get_flower_details_by_id
 
 
@@ -10,37 +11,43 @@ def client():
 
 
 def test_home_page(client):
+    """ Testing home_page """
     response = client.get('/')
     assert b"Welcome to the Flower Shop" in response.data
     assert response.status_code == 200
 
 
 def test_contact_page(client):
+    """ Testing contact_page """
     response = client.get('/contact')
     assert b"Contact Us" in response.data
     assert response.status_code == 200
 
 
 def test_flower_recommender_page(client):
+    """ Testing flower_recommender_page """
     response = client.get('/flower_recommender')
     assert b"Flower Recommender" in response.data
     assert response.status_code == 200
 
 
 def test_flower_details_page(client):
-    flower_id = 1  # Assuming there is a flower with ID 1 in your flower_data
+    """ Testing flower_details_page """
+    flower_id = 1  # Assuming there is a flower with ID 1
     response = client.get(f'/product-details/{flower_id}')
     assert b"Flower Details" in response.data
     assert response.status_code == 200
 
 
 def test_invalid_flower_details_page(client):
-    flower_id = 999  # Assuming there is no flower with ID 999 in your flower_data
+    """ Testing invalid_flower_details_page """
+    flower_id = 1999 
     response = client.get(f'/product-details/{flower_id}')
     assert response.status_code == 404
 
 
 def test_get_recommendations(client):
+    """ Testing get_recommendations """
     data = {
         'gender': 'female',
         'personality': 'romantic',
@@ -53,13 +60,13 @@ def test_get_recommendations(client):
 
 
 def test_filter_flowers():
-    # Add more test cases based on your filter_flowers function
+    """ Testing filter_flowers function """
     filtered_flowers = filter_flowers('female', 'romantic', 'anniversary', '50.0')
     assert len(filtered_flowers) > 0
 
 
 def test_get_flower_details_by_id():
-    # Add more test cases based on your flower data and IDs
+    """ Testing flower data and IDs """
     flower = get_flower_details_by_id(1, flower_data)
     assert flower is not None
     assert flower['productId'] == 1
@@ -67,4 +74,3 @@ def test_get_flower_details_by_id():
 
 if __name__ == '__main__':
     pytest.main()
-
